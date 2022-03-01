e61b8caf57a7b8c66c748d0526c219c3d5e7a4e9
Lasse <lassetange@gmail.com>
2022-03-01 22:09:29 +0100

    Test vars

--- test/blah.test.ts
+++ test/blah.test.ts
@@ -1,8 +1,10 @@
 import { run } from '../src';
+const fs = require('fs');
 
 describe('blah', () => {
   it('works', () => {
-    console.log(process.env);
+    const event = JSON.parse(fs.readFileSync(process.env.GITHUB_EVENT_PATH, 'utf8'));
+    console.log(event);
     expect(typeof run).toEqual('function');
   });
 });
