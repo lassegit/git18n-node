4559cbb43c3701aaad1a1d4f2b8a78905f78fb8d
Lasse <lassetange@gmail.com>
2022-03-01 21:44:46 +0100

    Show all process

--- test/blah.test.ts
+++ test/blah.test.ts
@@ -2,7 +2,6 @@ import { run } from '../src';
 
 describe('blah', () => {
   it('works', () => {
-    const vars = process.env;
     console.log(process.env);
 
     expect(typeof run).toEqual('function');
