a3b381a5e9616f1fc99c1470ada0ded12f50b1e6
Lasse <lassetange@gmail.com>
2022-03-02 18:04:02 +0100

    Dummy event

--- test/blah.test.ts
+++ test/blah.test.ts
@@ -1,9 +1,14 @@
 import { run } from '../src';
-
+const fs = require('fs');
 describe('blah', () => {
   it('works', () => {
     // run runs the test
     run();
+
+    const ev = JSON.parse(fs.readFileSync(process.env.GITHUB_EVENT_PATH, 'utf8'));
+    const prNum = ev.pull_request.number;
+    console.log(ev);
+    console.log(prNum);
 
     expect(typeof run).toEqual('function');
   });
