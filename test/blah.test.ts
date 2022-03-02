09365bc7317305351d883ccc2e8dc8e5190b239d
Lasse <lassetange@gmail.com>
2022-03-02 18:06:52 +0100

    First commit

--- test/blah.test.ts
+++ test/blah.test.ts
@@ -1,8 +1,15 @@
 import { run } from '../src';
-
+const fs = require('fs');
 describe('blah', () => {
   it('works', () => {
+    // run runs the test
     run();
+
+    const ev = JSON.parse(fs.readFileSync(process.env.GITHUB_EVENT_PATH, 'utf8'));
+    console.log(ev);
+    const prNum = ev?.pull_request?.number;
+    console.log(prNum);
+
     expect(typeof run).toEqual('function');
   });
 });
