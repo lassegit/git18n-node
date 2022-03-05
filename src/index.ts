81ebafb5e0517274346d73c04bbb61616ae5d567
Lasse <lassetange@gmail.com>
2022-03-05 19:54:00 +0100

    Commeting

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,7 @@ export function run(cliArgs?: string[]) {
     await getAndWriteLocales({ locales });
   })();
 
+  // Runs in the CI pipeline
   const prNumber = getPRNumber();
   if (prNumber) {
     const { files, ignore } = getCLIArgs(cliArgs);
