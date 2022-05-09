2b5c214f88a1b092337f490559929ddc6a80c37b
Lasse <lassetange@gmail.com>
2022-05-09 19:56:47 +0200

    Use official dotenv parse function

--- src/utils/getSecretAPIKey.ts
+++ src/utils/getSecretAPIKey.ts
@@ -1,46 +1,69 @@
 const fs = require('fs');
 const path = require('path');
 
-const { GIT18N_SECRET_PROJECT_KEY } = process.env;
+// .env parse function from dotenv code: https://github.com/motdotla/dotenv/blob/0318510821c578c5fa29697891ae922ee6d27a41/lib/main.js
+
+const LINE = /(?:^|^)\s*(?:export\s+)?([\w.-]+)(?:\s*=\s*?|:\s+?)(\s*'(?:\\'|[^'])*'|\s*"(?:\\"|[^"])*"|\s*`(?:\\`|[^`])*`|[^#\r\n]+)?\s*(?:#.*)?(?:$|$)/gm;
+
+function parse(src: Buffer) {
+  const obj: Record<string, string> = {};
+
+  // Convert buffer to string
+  let lines = src.toString();
+
+  // Convert line breaks to same format
+  lines = lines.replace(/\r\n?/gm, '\n');
+
+  let match;
+  while ((match = LINE.exec(lines)) != null) {
+    const key = match[1];
+
+    // Default undefined or null to empty string
+    let value = match[2] || '';
+
+    // Remove whitespace
+    value = value.trim();
+
+    // Check if double quoted
+    const maybeQuote = value[0];
+
+    // Remove surrounding quotes
+    value = value.replace(/^(['"`])([\s\S]*)\1$/gm, '$2');
+
+    // Expand newlines if double quoted
+    if (maybeQuote === '"') {
+      value = value.replace(/\\n/g, '\n');
+      value = value.replace(/\\r/g, '\r');
+    }
+
+    // Add to object
+    obj[key] = value;
+  }
+
+  return obj;
+}
 
 /**
  * Reads the project specific access token from either the process.env or .env file
  */
 export const getSecretAPIKey = (): string | undefined => {
-  if (GIT18N_SECRET_PROJECT_KEY) {
-    return GIT18N_SECRET_PROJECT_KEY;
+  if (process.env.GIT18N_SECRET_PROJECT_KEY) {
+    return process.env.GIT18N_SECRET_PROJECT_KEY;
   }
 
-  const file = path.resolve(process.cwd(), '.env');
-
-  if (!fs.existsSync(file)) {
-    throw new Error(`Couldn't find .env: ${file}`);
-  }
+  const filePath = path.resolve(process.cwd(), '.env');
 
   try {
-    const result = fs.readFileSync(file, 'utf8');
-    const lines = result.toString().split('\n');
-    const line = lines.find((line: string) => {
-      const match = line.match(/^([^=:#]+?)[=:](.*)/);
-      return match && match[1].includes('GIT18N_SECRET_PROJECT_KEY');
-    });
+    const parsed = parse(fs.readFileSync(filePath, 'utf8'));
 
-    if (!line) {
-      throw new Error(`Couldn't find GIT18N_SECRET_PROJECT_KEY in ${file}`);
+    if (!parsed.GIT18N_SECRET_PROJECT_KEY) {
+      throw new Error(`GIT18N_SECRET_PROJECT_KEY is not defined in .env file in ${filePath}`);
     }
 
-    const variable = line
-      .match(/^([^=:#]+?)[=:](.*)/)[2]
-      .trim()
-      .replace(/"/g, '')
-      .replace(/'/g, '');
-
-    if (!variable) {
-      throw new Error(`Couldn't parse GIT18N_SECRET_PROJECT_KEY correctly from ${file}`);
-    }
-
-    return variable;
+    return parsed.GIT18N_SECRET_PROJECT_KEY;
   } catch (error) {
-    throw error;
+    const message =
+      error instanceof Error ? error.message : typeof error === 'string' ? error : 'Unknown error';
+    throw new Error(message);
   }
 };
